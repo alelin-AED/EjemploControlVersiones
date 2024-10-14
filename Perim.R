e025644be8472700dc9efd32b58a4f61fdffa220
alelin-AED <alelin@alumni.uv.es>
2024-10-14 16:41:30 +0200

    return 2*(L+l)

--- Perim.R
+++ Perim.R
@@ -4,7 +4,11 @@ PerimetroCuadrado <- function(L){
 }
 
 LongitudCircunferencia <- function(R){
-  return(2*pi*R)
+  return(2*3.14159*R)
+}
+
+PerimetroRectangulo <- function(B,A){
+  return(B*A)
 }
 AreaTrapecio <- function(b,B,H){
   return(H*((b+B)/2))
